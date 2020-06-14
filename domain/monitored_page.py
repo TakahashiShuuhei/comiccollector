1bccea2ec6e07bf228f700e8d96d75a44c1ef8d7
shuhei.takahashi <shuhei.takahashi@brainpad.co.jp>
2020-06-14 20:13:52 +0900

    リンク先が相対リンクだったときに連結する

--- domain/monitored_page.py
+++ domain/monitored_page.py
@@ -4,6 +4,7 @@ from retrying import retry
 from domain.link import Link, CollectionType
 from typing import Set
 import re
+from urllib.parse import urlparse
 
 class MonitoredPage:
     """
@@ -27,14 +28,24 @@ class MonitoredPage:
         targets = soup.find_all(tag)
         p = re.compile(self.pattern)
         attr = self.type['attr']
-        return [target.attrs.get(attr) for target in targets if p.match(target.attrs.get(attr))]
+        return [self._convert_link(target.attrs.get(attr)) for target in targets if p.match(target.attrs.get(attr))]
 
     def check(self, link_repository) -> Set[Link]:
         found_links = self.collect()
-        saved_links = [link.url for link in link_repository.get_all(self.id)]
+        saved_links = [self._convert_link(link.url) for link in link_repository.get_all(self.id)]
         new_links = set(found_links) - set(saved_links)
         if new_links:
             new_links = {Link(self.id, l, self.type) for l in new_links}
             for link in new_links:
                 link_repository.save(link)
         return new_links
+    
+    def _convert_link(self, link):
+        if link.startswith('http'):
+            return link
+        url = self.url
+        if url.endswith('/'):
+            return url + link
+        return url[:url.rfind('/')] + '/' + link
+
+        
